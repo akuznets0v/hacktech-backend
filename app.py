b972fe5c54d24d3ef0a996b5a7fcd10e5b652539
akuznets0v <akuz.dev@gmail.com>
2016-02-27 21:53:31 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send, emit
+import json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# Server side
+@socketio.on('connect')
+def on_connect():
+    print 'client connected!'
+    
+    string = {"data":
+                [{"clients":
+                        [{"client0":
+                            [{"tl": [0,500] },
+                            {"tr": [100,500] },
+                            {"bl": [0,0] },
+                            {"br": [100,0] }]
+                        },
+                        {"client1":
+                            [{"tl": [200,500] },
+                            {"tr": [300,500] },
+                            {"bl": [200,0] },
+                            {"br": [300,0] }]
+                        }]
+                },
+                {"balls":
+                    {"0":
+                        {"position": [10,10]}
+                    }
+                }]
+            }
+            
+    emit('position', string)
+
+if __name__ == '__main__':
+    socketio.run(app)
